f276409e2fee4fade06175756dc35f9fb3f29c50
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2021-03-27 11:33:00 -0400

    feat: tcp_server.py finished

--- tcp_server.py
+++ tcp_server.py
@@ -5,3 +5,16 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
 port = 666
 
+s.bind((host, port))
+
+s.listen(3)
+
+while True:
+
+    conn, addr = s.accept()
+
+    print(addr, "Now Connected")
+
+    conn.send("Thank you for connecting")
+
+    conn.close()
