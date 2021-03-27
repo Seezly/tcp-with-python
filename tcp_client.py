4caea3bae9236eff8dff5739d278653ab309b064
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2021-03-27 11:44:09 -0400

    feat: tcp_client.py finished

---
+++ tcp_client.py
@@ -0,0 +1,14 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = socket.gethostname()
+port = 666
+
+s.connect((host, port))
+
+msg = s.recv(1024)
+
+s.close()
+
+print(msg.decode('ascii'))
